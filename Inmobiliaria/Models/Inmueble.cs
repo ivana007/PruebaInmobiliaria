7259e301f98df181f3480721f40e8942b30ffe73
Ivana007 <ivanalau31@gmail.com>
2020-04-04 15:58:58 -0300

    2 Commit agregue base de datos y diagrama

--- Inmobiliaria/Models/Inmueble.cs
+++ Inmobiliaria/Models/Inmueble.cs
@@ -7,5 +7,14 @@ namespace Inmobiliaria.Models
 {
     public class Inmueble
     {
+        [Key]
+        public int IdInmueble { get; set; }
+        public string Direccion { get; set; }
+        public string TipoImnueble { get; set; }
+        public string Precio { get; set; }
+        public string CantHambientes { get; set; }
+        public string Uso { get; set; }
+        public string Estado { get; set; }
+        public Propietario IdPropietario { get; set; }
     }
 }
